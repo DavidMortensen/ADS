cd53bb20715b951903e19fbff00d5680ce6cd61d
Nicolai Rendtslev <nre@caliodesign.com>
2018-02-08 21:26:27 +0100

    Made Isolate check, GiantComponent check and Connected Component check

--- 2/GiantBook.py
+++ 2/GiantBook.py
@@ -6,17 +6,15 @@ class UFW:
     if __name__ == '__main__':
         sites = stdio.readInt()
         uf = WeightedQuickUnionUF(sites)
+        print(uf._count)
+        uf.isNotIsolated()
+        
         while not stdio.isEmpty():
             s1 = stdio.readInt()
             s2 = stdio.readInt()
             uf.union(s1,s2)
-            for site in sites:
-                if uf.isNotIsolated():
-                    print('Isolated')
-                else: 
-                    print('Not Isolated')
-        
-        print('It is:', uf.connected(0,1), ' that 0 and 1 is connected')
-        
+        #print(uf._count, 'final number of components')
+        #print(uf.connected(0,1))
+        print(uf.isConnected())
         
 
--- 2/MyUnionFind.py
+++ 2/MyUnionFind.py
@@ -14,7 +14,6 @@ class WeightedQuickUnionUF:
 
     For additional documentation, see Section 1.5 of Algorithms, 4th Edition by Robert Sedgewick and Kevin Wayne.
     """
-
     def __init__(self, n):
         """
         Initializes an empty union-find data structure with n sites,
@@ -23,10 +22,16 @@ class WeightedQuickUnionUF:
         :param n: the number of sites
         """
         self._count = n
+        self._event = 0
+        self._notGiant = False
+        self._total = n
         self._parent = list(range(n))
-        self._isolated = 1
+        self._isolated = [1]*n
         self._size = [1]*n
-
+        self._countIsolated = n
+        self._isIsolated = True
+        self._connected = False
+   
     def _validate(self, p):
         # validate that p is a valid index
         n = len(self._parent)
@@ -34,6 +39,7 @@ class WeightedQuickUnionUF:
             raise ValueError('index {} is not between 0 and {}'.format(p, n))
 
     def union(self, p, q):
+        
         """
         Merges the component containing site p with the
         component containing site q.
@@ -41,10 +47,20 @@ class WeightedQuickUnionUF:
         :param p: the integer representing one site
         :param q: the integer representing the other site
         """
+        
         root_p = self.find(p)
         root_q = self.find(q)
         if root_p == root_q:
             return
+        
+        #Checks whether or not there's a giant component. If there is, print the 
+        #amount of unions made when it's created.
+        while not self._notGiant:
+            if self._event > self._total/2:
+                self._notGiant = True
+                print(self._event)
+            else:
+                    break
 
         # make root of smaller rank point to root of larger rank
         if self._size[root_p] < self._size[root_q]:
@@ -52,11 +68,22 @@ class WeightedQuickUnionUF:
         else:
             small, large = root_q, root_p
             
+        
         self._parent[small] = large
-        self._size[large] += small
+        self._size[large] += self._size[small]
       
         self._count -= 1
-        self._isolated = 0
+        
+        #Checks whether a component is isolated or not
+        if self._isolated[p] == 1:
+            self._isolated[p] = 0
+            self._countIsolated -= 1
+            
+        if self._isolated[q] == 1:
+            self._isolated[q] = 0
+            self._countIsolated -= 1
+        
+        
 
     def find(self, p):
         """
@@ -66,8 +93,11 @@ class WeightedQuickUnionUF:
         :return: the component identifier for the component containing site p
         """
         self._validate(p)
+        
         while p != self._parent[p]:
+            self._event += 1
             p = self._parent[p]
+            
         return p
 
     def connected(self, p, q):
@@ -79,10 +109,38 @@ class WeightedQuickUnionUF:
         :return: true if the two sites p and q are in the same component; false otherwise
         """
         return self.find(p) == self.find(q)
+    
 
     def count(self):
         return self._count
 
     def isNotIsolated(self):
-        return(self._isolated == 1)
-            
+        """
+        Returns the amount of unions made before the amount of isolated sites is 0.
+
+        If > 0, returns sufficient message.
+        """
+        while self._isIsolated: 
+            if 1 in self._isolated:
+                #print("There's still isolated sites")
+                break
+            else:
+                self._isIsolated = False
+                print(self._event)
+        return(self._event)
+    
+    def isConnected(self):
+        """ 
+        Returns True if there's one connected component. 
+        Returns False if there's several components.
+        """
+        if self._count == 1:
+            self._connected = True
+        #print(self._event)
+        return self._event
+        
+    #def isEvent(self):
+        #if not self.connected():
+         #   self._event += 1
+        #else: 
+        #    self._event += 1
