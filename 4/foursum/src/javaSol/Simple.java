1f33b1cd2180eccbb6f05cb232771881988aeb09
David Mortensen <davidmortensen@Davids-MacBook-Pro-2.local>
2018-02-21 15:48:53 +0100

    made simple versions of java and python

--- 4/foursum/src/javaSol/Simple.java
+++ 4/foursum/src/javaSol/Simple.java
@@ -7,16 +7,26 @@ public class Simple
 	Scanner S= new Scanner(System.in);
 	int N = Integer.parseInt(S.nextLine());
 	long[] vals = new long[N];
-	for(int i= 0; i < N; i+= 1) vals[i] = Long.parseLong(S.nextLine());
+	for(int i= 0; i < N; i+= 1) {
+		vals[i] = Long.parseLong(S.nextLine());
+	}
 
         // your code goes here and uses the following
-
-			if (vals[i] + vals[j] + vals[k] + vals[l] == 0) 
-			{
-			    System.err.println(i+" "+j+" "+k+" "+l);
-			    System.out.println(true);
-			    System.exit(0);
+		for(int i= 0; i< N; i+= 1) {
+			for(int j= 0; j< N; j+= 1) {
+				for(int k= 0; k< N; k+= 1) {
+					for(int l= 0; l< N; l+= 1) {
+						if (vals[i] + vals[j] + vals[k] + vals[l] == 0) 
+							{
+								System.err.println(i+" "+j+" "+k+" "+l);
+								System.out.println(true);
+								System.exit(0);
+							}
+					}
+				}
 			}
+		}
+			
         System.out.println(false);
     }
 }
--- 4/foursum/src/pythonSol/simple.py
+++ 4/foursum/src/pythonSol/simple.py
@@ -6,6 +6,9 @@ vals = list(map(int, sys.stdin.readlines()))
 
 for i in range(0, N):
     # your code goes here and uses the following
+    for j in range(0, N):
+        for k in range(0, N):
+            for l in range(0, N):
                 if vals[i]+vals[j]+vals[k]+vals[l] == 0:
                     print(i,j,k,l,file=sys.stderr)
                     print(True)
